c7ae35f184d87c5260566bed13e5f827b1f8d5ee
Yanghyeonyong <didgusdyd2@gmail.com>
2026-01-13 13:06:45 +0900

    [Add] 테스트용 로그인 없이 넘어가는 기능 추가

--- Assets/Scripts/Hyeonyong/Network/ServerConnector.cs
+++ Assets/Scripts/Hyeonyong/Network/ServerConnector.cs
@@ -3,6 +3,7 @@ using Photon.Pun;
 using UnityEngine.SceneManagement;
 public class ServerConnector : MonoBehaviourPunCallbacks
 {
+    [SerializeField] bool onTest = false;
     private void Awake()
     {
         //호스트가 씬 이동시 클라이언트도 같이 이동
@@ -20,8 +21,15 @@ public class ServerConnector : MonoBehaviourPunCallbacks
     public override void OnConnectedToMaster()
     {
         Debug.Log("마스터 서버 연동 성공");
-       // PhotonNetwork.JoinLobby();//로비 입장을 시키는 명령
-        SceneManager.LoadScene("Login");//싱글톤도 아니고 dontdestroy 아니면 씬 넘어갈 경우 해당 스크립트 파괴
+        // PhotonNetwork.JoinLobby();//로비 입장을 시키는 명령
+        if (onTest)
+        {
+            SceneManager.LoadScene("Lobby");
+        }
+        else
+        {
+            SceneManager.LoadScene("Login");//싱글톤도 아니고 dontdestroy 아니면 씬 넘어갈 경우 해당 스크립트 파괴
+        }
     }
     //public override void OnJoinedLobby()
     //{
