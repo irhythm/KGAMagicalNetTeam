265a6123a29676400634af984f4126e1e9bef703
firedHouse <y.hyungu4704@gmail.com>
2026-01-14 01:21:57 +0900

    [feat] 시민 AI 각 상태 구현
    Patrol -> 순찰
    Alert -> 공포
    Action -> 도주
    각 상태별 로직 구현

---
+++ Assets/Scripts/YHG/CitizenAlertState.cs
@@ -0,0 +1,52 @@
+﻿using UnityEngine;
+//공포상태
+public class CitizenAlertState : AIStateBase
+{
+    private CitizenAI citizen; //시민ㄴ변수 접근
+
+    //alert 전달
+    public CitizenAlertState(BaseAI ai, StateMachine stateMachine) 
+        : base(ai, stateMachine, BaseAI.AIStateID.Alert)
+    {
+        citizen = ai as CitizenAI;
+    }
+
+    public override void Enter()
+    {
+        base.Enter(); //네트워크/애니동기화
+
+        //멈추고
+        citizen.Agent.isStopped = true;
+
+        //룩앳 추가
+        if (citizen.detectedPlayer != null)
+        {
+            citizen.transform.LookAt(citizen.detectedPlayer);
+        }
+    }
+    public override void Exit()
+    {
+        //액션 가기 전에 멈춤 해제
+        citizen.Agent.isStopped = false;
+    }
+    public override void Execute()
+    {
+        //플레이어 나가면 다시 패트롤
+        if (citizen.detectedPlayer == null)
+        {
+            stateMachine.ChangeState(new CitizenPatrolState(citizen, stateMachine));
+            return;
+        }
+
+        //플레이어 거리 계산
+        float distSqr = (citizen.transform.position - citizen.detectedPlayer.position).sqrMagnitude;
+        //감지 범위의 제곱
+        float safeDistSqr = citizen.detectRadius * citizen.detectRadius;
+
+        if (distSqr > safeDistSqr)
+        {
+            //역돌격 상태 시작
+            stateMachine.ChangeState(new CitizenActionState(citizen, stateMachine));
+        }
+    }
+}
---
+++ Assets/Scripts/YHG/CitizenPatrolState.cs
@@ -0,0 +1,53 @@
+﻿using UnityEngine;
+
+public class CitizenPatrolState : AIStateBase
+{
+    private CitizenAI citizen;      
+    private bool movingForward = true; //앞뒤전환
+
+    public override void Execute()
+    {
+        //플레이어가 근처에 있는지 체크
+        if (citizen.CheckPlayerNearby())
+        {
+            //공포상태로 전환
+            stateMachine.ChangeState(new CitizenAlertState(citizen, stateMachine));
+            return;
+        }
+
+        //목적지에 도착했는지 체크
+        //네비매쉬 pathPending: 경로 계산 중인가? 트루면 계싼중인거
+        //remainingDistance: 남은 거리
+        if (!citizen.Agent.pathPending && citizen.Agent.remainingDistance < 0.5f)
+        {
+            //도착했으면 반대로 뒤집고 다시 이동
+            movingForward = !movingForward;
+            MoveToNextPoint();
+        }
+    }
+    //BaseAi로 받아서 CitizenAI로 변경하기(citizenAI 변수쓰려고)
+    public CitizenPatrolState(BaseAI ai, StateMachine machine): base(ai, machine, BaseAI.AIStateID.Patrol)
+    {
+        citizen = ai as CitizenAI;
+    }
+
+    public override void Enter()
+    {
+        //ChangeNetworkState 용 필수
+        base.Enter(); 
+
+        citizen.Agent.speed = citizen.moveSpeed; //걷는 속도로 설정 
+        citizen.Agent.isStopped = false;         //이동 시작 
+
+        MoveToNextPoint(); //전진 후진 시작
+    }
+    //다음 이동 지점 설정
+    private void MoveToNextPoint()
+    {
+        //CitizenAI 본체에게 순찰 지점 좌표 요청
+        Vector3 target = citizen.GetPatrolPoint(movingForward);
+
+        //네비게이션 에이전트에게 이동 명령
+        citizen.Agent.SetDestination(target);
+    }
+}
