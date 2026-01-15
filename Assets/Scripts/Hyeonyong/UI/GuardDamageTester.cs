22b5f0144f37f1e5f46c6769a38d5bb9ef8206a7
firedHouse <y.hyungu4704@gmail.com>
2026-01-15 18:22:04 +0900

    [feat] 가드매니저테스터수정

--- Assets/Scripts/Hyeonyong/UI/GuardDamageTester.cs
+++ Assets/Scripts/Hyeonyong/UI/GuardDamageTester.cs
@@ -1,30 +1,40 @@
 using UnityEngine;
 using Photon.Pun;
+using UnityEngine.InputSystem;
 
 public class GuardDamageTester : MonoBehaviour
 {
     private GuardAI guard;
+    private InputAction testAction;
+
+    private void Awake()
+    {
+        guard = GetComponent<GuardAI>();
+
+        //t키 바인딩
+        testAction = new InputAction(binding: "<Keyboard>/t");
+    }
 
     void Start()
     {
         guard = GetComponent<GuardAI>();
     }
 
-    void Update()
+    private void OnEnable()
     {
-        if (!PhotonNetwork.IsMasterClient) return;
-
-        if (Input.GetKeyDown(KeyCode.T))
+        testAction.Enable(); 
+        testAction.performed += OnTestInput;
+    }
+    private void OnTestInput(InputAction.CallbackContext ctx)
+    {
+        if (guard != null && guard.targetPlayer != null)
         {
-            if (guard.targetPlayer != null)
-            {
-                Debug.Log($"T키 입력 -> InflictDamage() 강제 호출");
-                guard.InflictDamage();
-            }
-            else
-            {
-                Debug.Log("타겟 플레이어가 없음");
-            }
+            Debug.Log($"T 입력 -> InflictDamage() 강제 호출");
+            guard.InflictDamage();
+        }
+        else
+        {
+            Debug.Log("GuardAI를 찾을 수 없음");
         }
     }
 }
