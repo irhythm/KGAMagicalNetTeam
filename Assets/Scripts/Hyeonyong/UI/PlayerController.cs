2076d5f0e2796604005c36ce5876881d13b676ba
GodKong2431 <kizna10231@gmail.com>
2026-01-19 05:44:15 +0900

    [Edit]]인벤토리 UI 이벤트 시스템으로 리펙토링
    인벤토리 UI 이벤트 시스템으로 리펙토링

--- Assets/Scripts/Hyeonyong/UI/PlayerController.cs
+++ Assets/Scripts/Hyeonyong/UI/PlayerController.cs
@@ -3,7 +3,6 @@ using Photon.Voice.PUN;
 using TMPro;
 using UnityEngine;
 using UnityEngine.InputSystem;
-using UnityEngine.Profiling;
 using UnityEngine.UI;
 
 public class PlayerController : MonoBehaviourPun, IDamageable
@@ -11,12 +10,9 @@ public class PlayerController : MonoBehaviourPun, IDamageable
     [SerializeField] private InputActionReference testTakeDamageAction;
     PhotonView pv;
     PhotonVoiceView pvv;
-    Recorder recorder;
 
     private PlayableCharacter playableCharacter;
-
     public PlayerView playerView;
-
 
     [SerializeField] GameObject playerInfoPrefab;
     [SerializeField] GameObject myInfoPrefab;
@@ -25,127 +21,111 @@ public class PlayerController : MonoBehaviourPun, IDamageable
     [SerializeField] Transform playerInfoPanel;
     [SerializeField] Transform myInfoPanel;
     [SerializeField] Transform magicInfoPanel;
+
     GameObject playerInfo;
     GameObject magicInfo;
 
-    //[SerializeField] Image speakerImage;
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
         pv = GetComponent<PhotonView>();
         pvv = GetComponent<PhotonVoiceView>();
-
         playableCharacter = GetComponent<PlayableCharacter>();
 
         SubscribeEvents();
 
         if (pv.IsMine)
         {
-            if (myInfoPanel == null)
+            if (myInfoPanel == null) 
                 myInfoPanel = UIManager.Instance.myInfoPanel;
-            if(magicInfoPanel == null)
+
+            if (magicInfoPanel == null) 
                 magicInfoPanel = UIManager.Instance.IconPanel;
+
             playerInfo = Instantiate(myInfoPrefab, myInfoPanel);
             SetMyInfo();
+
             magicInfo = Instantiate(magicInfoPrefab, magicInfoPanel);
             SetMagicInfo();
             SetHandInfo();
 
             playerView.UpdatePlayerHP(1f);
+
+            PlayerMagicSystem magicSystem = GetComponent<PlayerMagicSystem>();
+            if (magicSystem != null)
+            {
+                playerView.BindMagicSystem(magicSystem, playableCharacter);
+            }
         }
         else
         {
-            if (playerInfoPanel == null)
+            if (playerInfoPanel == null) 
                 playerInfoPanel = UIManager.Instance.playerInfoPanel;
+
             playerInfo = Instantiate(playerInfoPrefab, playerInfoPanel);
             SetPlayerInfo();
-
             playerView.UpdatePlayerHP(1f);
         }
+
         SetPlayerName(pv.Owner.NickName);
 
         testTakeDamageAction.action.Enable();
         testTakeDamageAction.action.performed += TestTakeDamage;
-
-        //playerView.SetVoiceImage(speakerImage);
     }
 
-    // 구독 해제
     private void OnDisable()
     {
         UnsubscribeEvents();
-        Destroy(playerInfo);
+        if (playerInfo != null) 
+            Destroy(playerInfo);
     }
 
-    // 이벤트 구독 호출
     private void SubscribeEvents()
     {
-        if (playableCharacter != null)
-        {
+        if (playableCharacter != null) 
             playableCharacter.OnHpChanged += HandleHpChanged;
-            // playableCharacter.OnDie += HandleDie; // 이벤트 필요하면 여기에서 연결
-        }
     }
 
-    // 구독 해제 호출
     private void UnsubscribeEvents()
     {
-        if (playableCharacter != null)
-        {
+        if (playableCharacter != null) 
             playableCharacter.OnHpChanged -= HandleHpChanged;
-            // playableCharacter.OnDie -= HandleDie; // 죽었을 때 뭐 할거면 구현해야할 듯?
-        }
     }
 
-    // 업데이트시 호출
     private void HandleHpChanged(float hpRatio)
     {
         playerView.UpdatePlayerHP(hpRatio);
     }
 
-
     public void TestTakeDamage(InputAction.CallbackContext context)
     {
-        if (!pv.IsMine)
-            return;
-
+        if (!pv.IsMine) return;
         TakeDamage(10f);
-        //pv.RPC(nameof(OnTakeDamageRPC), RpcTarget.All, 10f);
-        //pv.RPC(nameof(UsingMagic),RpcTarget.All);
-
     }
 
     public void TakeDamage(float takeDamage)
     {
-        if (!pv.IsMine)
-            return;
-
+        if (!pv.IsMine) return;
         playableCharacter.OnAttacked(takeDamage);
-
-        // pv.RPC(nameof(OnTakeDamageRPC), RpcTarget.All, 10f);
     }
-
 
     public void SetPlayerInfo()
     {
         playerView.SetPlayerInfo(
-playerInfo.transform.GetChild(0).GetComponent<TextMeshProUGUI>(),
-playerInfo.transform.GetChild(1).GetComponent<Image>(),
-playerInfo.transform.GetChild(2).GetComponent<Image>()
-);
+            playerInfo.transform.GetChild(0).GetComponent<TextMeshProUGUI>(),
+            playerInfo.transform.GetChild(1).GetComponent<Image>(),
+            playerInfo.transform.GetChild(2).GetComponent<Image>()
+        );
     }
     public void SetMyInfo()
     {
         playerView.SetMyInfo(
-playerInfo.transform.GetChild(0).GetComponent<TextMeshProUGUI>(),
-playerInfo.transform.GetChild(1).GetComponent<Image>()
-);
+            playerInfo.transform.GetChild(0).GetComponent<TextMeshProUGUI>(),
+            playerInfo.transform.GetChild(1).GetComponent<Image>()
+        );
     }
-
 
     public void SetMagicInfo()
     {
-        //크기가 정해져 있어서 리스트나 큐 대신 배열 사용
         int count = magicInfo.transform.GetChild(2).childCount;
         GameObject[] magic = new GameObject[count];
         for (int i = 0; i < count; i++)
@@ -163,35 +143,6 @@ playerInfo.transform.GetChild(1).GetComponent<Image>()
         playerView.SetMagicInfoOnHand(hand);
     }
 
-    public void CheckPlayerName(TextMeshProUGUI name)
-    {
-        if (name == null)
-        {
-            Debug.Log("이름 없음");
-        }
-        else
-        {
-            Debug.Log("이름 있음 : " + name.text);
-        }
-    }
-    public void CheckPlayerImage(Image hp)
-    {
-        if (hp == null)
-        {
-            Debug.Log("이미지 없음");
-        }
-        else
-        {
-            Debug.Log("이미지 있음 : " + hp.fillAmount);
-        }
-    }
-
-    // 이벤트로 바꿔서 호출 이제 필요없을 듯?
-    public void UpdatePlayerHp()
-    {
-        // playerView.UpdatePlayerHP(playerModel.CurHp / playerModel.MaxHp);
-    }
-
     public void SetPlayerName(string name)
     {
         playerView.SetPlayerName(name);
@@ -204,42 +155,251 @@ playerInfo.transform.GetChild(1).GetComponent<Image>()
             playerView.CheckVoiceImage(pvv.IsSpeaking);
         }
     }
-
-    //public void UsingMagic(MagicDataSO magicData)
-    //{
-    //    playerView.SetMagicIcon(magicData.itemImage, magicData.cooldown);
-    //}
-
-    public void SetItem(InventoryDataSO item, bool isLeft)
-    {
-        playerView.SetIconOnHand(item, isLeft);
-    }
-
-    public void SetCoolTime(MagicBase magic, bool isLeft)
-    {
-        playerView.CheckCoolTimeOnHand(magic, isLeft);
-    }
-
-
-    public void SetMagicIcon(InventoryDataSO data, PlayableCharacter player)
-    {
-        if(data == null)
-            return;
-
-        MagicDataSO magicData = data as MagicDataSO;
-        if(magicData == null) 
-            return;
-
-        MagicBase targetLogic = player.Inventory.GetMagicInstance(magicData);
-
-        if (targetLogic == null)
-        {
-            targetLogic = magicData.CreateInstance();
-        }
-        if (targetLogic.CurrentCooldown <= 0)
-            return;
-        playerView.SetMagicIcon(targetLogic);
-
-    }
-
 }
+#region 레거시 코드
+//using Photon.Pun;
+//using Photon.Voice.PUN;
+//using TMPro;
+//using UnityEngine;
+//using UnityEngine.InputSystem;
+//using UnityEngine.Profiling;
+//using UnityEngine.UI;
+
+//public class PlayerController : MonoBehaviourPun, IDamageable
+//{
+//    [SerializeField] private InputActionReference testTakeDamageAction;
+//    PhotonView pv;
+//    PhotonVoiceView pvv;
+//    Recorder recorder;
+
+//    private PlayableCharacter playableCharacter;
+
+//    public PlayerView playerView;
+
+
+//    [SerializeField] GameObject playerInfoPrefab;
+//    [SerializeField] GameObject myInfoPrefab;
+//    [SerializeField] GameObject magicInfoPrefab;
+
+//    [SerializeField] Transform playerInfoPanel;
+//    [SerializeField] Transform myInfoPanel;
+//    [SerializeField] Transform magicInfoPanel;
+//    GameObject playerInfo;
+//    GameObject magicInfo;
+
+//    //[SerializeField] Image speakerImage;
+//    // Start is called once before the first execution of Update after the MonoBehaviour is created
+//    void Start()
+//    {
+//        pv = GetComponent<PhotonView>();
+//        pvv = GetComponent<PhotonVoiceView>();
+
+//        playableCharacter = GetComponent<PlayableCharacter>();
+
+//        SubscribeEvents();
+
+//        if (pv.IsMine)
+//        {
+//            if (myInfoPanel == null)
+//                myInfoPanel = UIManager.Instance.myInfoPanel;
+//            if(magicInfoPanel == null)
+//                magicInfoPanel = UIManager.Instance.IconPanel;
+//            playerInfo = Instantiate(myInfoPrefab, myInfoPanel);
+//            SetMyInfo();
+//            magicInfo = Instantiate(magicInfoPrefab, magicInfoPanel);
+//            SetMagicInfo();
+//            SetHandInfo();
+
+//            playerView.UpdatePlayerHP(1f);
+//        }
+//        else
+//        {
+//            if (playerInfoPanel == null)
+//                playerInfoPanel = UIManager.Instance.playerInfoPanel;
+//            playerInfo = Instantiate(playerInfoPrefab, playerInfoPanel);
+//            SetPlayerInfo();
+
+//            playerView.UpdatePlayerHP(1f);
+//        }
+//        SetPlayerName(pv.Owner.NickName);
+
+//        testTakeDamageAction.action.Enable();
+//        testTakeDamageAction.action.performed += TestTakeDamage;
+
+//        //playerView.SetVoiceImage(speakerImage);
+//    }
+
+//    // 구독 해제
+//    private void OnDisable()
+//    {
+//        UnsubscribeEvents();
+//        Destroy(playerInfo);
+//    }
+
+//    // 이벤트 구독 호출
+//    private void SubscribeEvents()
+//    {
+//        if (playableCharacter != null)
+//        {
+//            playableCharacter.OnHpChanged += HandleHpChanged;
+//            // playableCharacter.OnDie += HandleDie; // 이벤트 필요하면 여기에서 연결
+//        }
+//    }
+
+//    // 구독 해제 호출
+//    private void UnsubscribeEvents()
+//    {
+//        if (playableCharacter != null)
+//        {
+//            playableCharacter.OnHpChanged -= HandleHpChanged;
+//            // playableCharacter.OnDie -= HandleDie; // 죽었을 때 뭐 할거면 구현해야할 듯?
+//        }
+//    }
+
+//    // 업데이트시 호출
+//    private void HandleHpChanged(float hpRatio)
+//    {
+//        playerView.UpdatePlayerHP(hpRatio);
+//    }
+
+
+//    public void TestTakeDamage(InputAction.CallbackContext context)
+//    {
+//        if (!pv.IsMine)
+//            return;
+
+//        TakeDamage(10f);
+//        //pv.RPC(nameof(OnTakeDamageRPC), RpcTarget.All, 10f);
+//        //pv.RPC(nameof(UsingMagic),RpcTarget.All);
+
+//    }
+
+//    public void TakeDamage(float takeDamage)
+//    {
+//        if (!pv.IsMine)
+//            return;
+
+//        playableCharacter.OnAttacked(takeDamage);
+
+//        // pv.RPC(nameof(OnTakeDamageRPC), RpcTarget.All, 10f);
+//    }
+
+
+//    public void SetPlayerInfo()
+//    {
+//        playerView.SetPlayerInfo(
+//playerInfo.transform.GetChild(0).GetComponent<TextMeshProUGUI>(),
+//playerInfo.transform.GetChild(1).GetComponent<Image>(),
+//playerInfo.transform.GetChild(2).GetComponent<Image>()
+//);
+//    }
+//    public void SetMyInfo()
+//    {
+//        playerView.SetMyInfo(
+//playerInfo.transform.GetChild(0).GetComponent<TextMeshProUGUI>(),
+//playerInfo.transform.GetChild(1).GetComponent<Image>()
+//);
+//    }
+
+
+//    public void SetMagicInfo()
+//    {
+//        //크기가 정해져 있어서 리스트나 큐 대신 배열 사용
+//        int count = magicInfo.transform.GetChild(2).childCount;
+//        GameObject[] magic = new GameObject[count];
+//        for (int i = 0; i < count; i++)
+//        {
+//            magic[i] = magicInfo.transform.GetChild(2).GetChild(i).gameObject;
+//        }
+//        playerView.SetMagicInfo(magic);
+//    }
+
+//    public void SetHandInfo()
+//    {
+//        GameObject[] hand = new GameObject[2];
+//        hand[0] = magicInfo.transform.GetChild(0).gameObject;
+//        hand[1] = magicInfo.transform.GetChild(1).gameObject;
+//        playerView.SetMagicInfoOnHand(hand);
+//    }
+
+//    public void CheckPlayerName(TextMeshProUGUI name)
+//    {
+//        if (name == null)
+//        {
+//            Debug.Log("이름 없음");
+//        }
+//        else
+//        {
+//            Debug.Log("이름 있음 : " + name.text);
+//        }
+//    }
+//    public void CheckPlayerImage(Image hp)
+//    {
+//        if (hp == null)
+//        {
+//            Debug.Log("이미지 없음");
+//        }
+//        else
+//        {
+//            Debug.Log("이미지 있음 : " + hp.fillAmount);
+//        }
+//    }
+
+//    // 이벤트로 바꿔서 호출 이제 필요없을 듯?
+//    public void UpdatePlayerHp()
+//    {
+//        // playerView.UpdatePlayerHP(playerModel.CurHp / playerModel.MaxHp);
+//    }
+
+//    public void SetPlayerName(string name)
+//    {
+//        playerView.SetPlayerName(name);
+//    }
+
+//    void Update()
+//    {
+//        if (!pv.IsMine)
+//        {
+//            playerView.CheckVoiceImage(pvv.IsSpeaking);
+//        }
+//    }
+
+//    //public void UsingMagic(MagicDataSO magicData)
+//    //{
+//    //    playerView.SetMagicIcon(magicData.itemImage, magicData.cooldown);
+//    //}
+
+//    public void SetItem(InventoryDataSO item, bool isLeft)
+//    {
+//        playerView.SetIconOnHand(item, isLeft);
+//    }
+
+//    public void SetCoolTime(MagicBase magic, bool isLeft)
+//    {
+//        playerView.CheckCoolTimeOnHand(magic, isLeft);
+//    }
+
+
+//    public void SetMagicIcon(InventoryDataSO data, PlayableCharacter player)
+//    {
+//        if(data == null)
+//            return;
+
+//        MagicDataSO magicData = data as MagicDataSO;
+//        if(magicData == null) 
+//            return;
+
+//        MagicBase targetLogic = player.Inventory.GetMagicInstance(magicData);
+
+//        if (targetLogic == null)
+//        {
+//            targetLogic = magicData.CreateInstance();
+//        }
+//        if (targetLogic.CurrentCooldown <= 0)
+//            return;
+//        playerView.SetMagicIcon(targetLogic);
+
+//    }
+
+//}
+#endregion
--- Assets/Scripts/Hyeonyong/UI/PlayerView.cs
+++ Assets/Scripts/Hyeonyong/UI/PlayerView.cs
@@ -1,6 +1,5 @@
 using System.Collections;
 using TMPro;
-using Unity.VisualScripting;
 using UnityEngine;
 using UnityEngine.UI;
 
@@ -16,9 +15,13 @@ public class PlayerView : MonoBehaviour
     Image leftHandIconCoolTime;
     Image rightHandIcon;
     Image rightHandIconCoolTime;
+
+    private PlayerMagicSystem _boundSystem;
+    private PlayableCharacter _player;
+
     public void SetVoiceImage(Image photonVoiceImage)
     {
-        voiceImage = photonVoiceImage;
+        voiceImage = photonVoiceImage; 
         voiceImage.enabled = false;
     }
 
@@ -29,13 +32,14 @@ public class PlayerView : MonoBehaviour
 
     public void SetPlayerInfo(TextMeshProUGUI name, Image hp, Image voice)
     {
-        playerName = name;
-        playerHp = hp;
+        playerName = name; 
+        playerHp = hp; 
         voiceImage = voice;
     }
+
     public void SetMyInfo(TextMeshProUGUI name, Image hp)
     {
-        playerName = name;
+        playerName = name; 
         playerHp = hp;
     }
 
@@ -43,6 +47,7 @@ public class PlayerView : MonoBehaviour
     {
         playerName.text = name;
     }
+
     public void UpdatePlayerHP(float amount)
     {
         playerHp.fillAmount = amount;
@@ -63,68 +68,116 @@ public class PlayerView : MonoBehaviour
         rightHandIconCoolTime = magic[1].transform.GetChild(0).GetComponent<Image>();
     }
 
-    public GameObject GetMagicIcon()
+    public void BindMagicSystem(PlayerMagicSystem system, PlayableCharacter player)
     {
-        foreach (var i in icon)
+        if (_boundSystem != null) 
+            UnsubscribeEvents();
+
+        _boundSystem = system;
+        _player = player;
+
+        if (_boundSystem != null) 
+            SubscribeEvents();
+    }
+
+    private void OnDisable()
+    {
+        UnsubscribeEvents();
+    }
+
+    private void SubscribeEvents()
+    {
+        if (_boundSystem == null) return;
+        _boundSystem.OnHandItemChanged += SetIconOnHand;
+        _boundSystem.OnHandCooldownStarted += CheckCoolTimeOnHand;
+        _boundSystem.OnInventoryCooldownCheck += HandleInventoryCooldownCheck;
+    }
+
+    private void UnsubscribeEvents()
+    {
+        if (_boundSystem == null) return;
+        _boundSystem.OnHandItemChanged -= SetIconOnHand;
+        _boundSystem.OnHandCooldownStarted -= CheckCoolTimeOnHand;
+        _boundSystem.OnInventoryCooldownCheck -= HandleInventoryCooldownCheck;
+    }
+
+    // 인벤토리 아이템 쿨타임 확인용
+    private void HandleInventoryCooldownCheck(InventoryDataSO data)
+    {
+        if (data == null || _player == null) return;
+
+        MagicDataSO magicData = data as MagicDataSO;
+        if (magicData == null) return;
+
+        MagicBase targetLogic = _player.Inventory.GetMagicInstance(magicData);
+
+        if (targetLogic != null && targetLogic.CurrentCooldown > 0)
         {
-            if (!i.activeSelf)
-                return i;
+            SetMagicIcon(targetLogic);
         }
-        return null;
     }
 
     public void SetIconOnHand(InventoryDataSO data, bool isLeft)
     {
-        //260116 손 비우기 용 코드 최정욱
         if (data == null)
         {
-            if (isLeft)
-            {
-                leftHandIcon.sprite = null;
-                leftHandIconCoolTime.fillAmount = 0;
+            if (isLeft) 
+            { 
+                leftHandIcon.sprite = null; 
+                leftHandIconCoolTime.fillAmount = 0; 
             }
-            else
-            {
-                rightHandIcon.sprite = null;
-                rightHandIconCoolTime.fillAmount = 0;
+            else 
+            { 
+                rightHandIcon.sprite = null; 
+                rightHandIconCoolTime.fillAmount = 0; 
             }
             return;
         }
 
-        if (data.itemImage == null)
+        if (data.itemImage == null) 
             return;
-        if (isLeft)
-        {
-            leftHandIcon.sprite = data.itemImage;
-            leftHandIconCoolTime.fillAmount = 0;
-        }
-        else
-        {
-            rightHandIcon.sprite = data.itemImage;
-            rightHandIconCoolTime.fillAmount = 0;
-        }
-            //iconOnHand[num].GetComponent<Image>().sprite = data.itemImage;
 
+        if (isLeft) 
+        { 
+            leftHandIcon.sprite = data.itemImage; 
+            leftHandIconCoolTime.fillAmount = 0; 
+        }
+        else 
+        { 
+            rightHandIcon.sprite = data.itemImage; 
+            rightHandIconCoolTime.fillAmount = 0; 
+        }
     }
+
     public void CheckCoolTimeOnHand(MagicBase magic, bool isLeft)
     {
+        if (magic == null) return;
+
         float curCoolTime = magic.CurrentCooldown;
         float maxCoolTime = magic.Data.cooldown;
 
-        Image coolTimeIcon = null;
-        if (isLeft)
-            coolTimeIcon = leftHandIconCoolTime;
-        else
-            coolTimeIcon = rightHandIconCoolTime;
+        Image coolTimeIcon = isLeft ? leftHandIconCoolTime : rightHandIconCoolTime;
 
         coolTimeIcon.fillAmount = curCoolTime / maxCoolTime;
         StartCoroutine(CheckCoolTimeOnHand(coolTimeIcon, curCoolTime, maxCoolTime));
     }
 
+    public GameObject GetMagicIcon()
+    {
+        foreach (var i in icon)
+        {
+            if (!i.activeSelf) 
+                return i;
+        }
+        return null;
+    }
 
     public void SetMagicIcon(MagicBase magic)
     {
         GameObject magicIcon = GetMagicIcon();
+        if (magicIcon == null) 
+            return;
+
         magicIcon.GetComponent<Image>().sprite = magic.Data.itemImage;
         Image coolTimeImage = magicIcon.transform.GetChild(0).GetComponent<Image>();
 
@@ -132,20 +185,17 @@ public class PlayerView : MonoBehaviour
         float maxCoolTime = magic.Data.cooldown;
         coolTimeImage.fillAmount = curCoolTime / maxCoolTime;
         magicIcon.SetActive(true);
-        StartCoroutine(CheckCoolTimeOnInventory(coolTimeImage,curCoolTime,maxCoolTime, magicIcon));        
+        StartCoroutine(CheckCoolTimeOnInventory(coolTimeImage, curCoolTime, maxCoolTime, magicIcon));
     }
-
-
 
     public IEnumerator CheckCoolTimeOnHand(Image checkCoolDown, float curCoolTime, float maxCoolTime)
     {
         while (curCoolTime > 0)
         {
-            if (curCoolTime > checkDuration)
-                yield return CoroutineManager.waitForSeconds(checkDuration);
-            else
-                yield return CoroutineManager.waitForSeconds(curCoolTime);
-            curCoolTime -= checkDuration;
+            float waitTime = (curCoolTime > checkDuration) ? checkDuration : curCoolTime;
+            yield return CoroutineManager.waitForSeconds(waitTime);
+
+            curCoolTime -= waitTime;
             checkCoolDown.fillAmount = curCoolTime / maxCoolTime;
         }
     }
@@ -154,11 +204,10 @@ public class PlayerView : MonoBehaviour
     {
         while (curCoolTime > 0)
         {
-            if (curCoolTime > checkDuration)
-                yield return CoroutineManager.waitForSeconds(checkDuration);
-            else
-                yield return CoroutineManager.waitForSeconds(curCoolTime);
-            curCoolTime -= checkDuration;
+            float waitTime = (curCoolTime > checkDuration) ? checkDuration : curCoolTime;
+            yield return CoroutineManager.waitForSeconds(waitTime);
+
+            curCoolTime -= waitTime;
             checkCoolDown.fillAmount = curCoolTime / maxCoolTime;
         }
         if (magicIcon != null)
@@ -167,3 +216,175 @@ public class PlayerView : MonoBehaviour
         }
     }
 }
+
+#region 레거시 코드
+//using System.Collections;
+//using TMPro;
+//using Unity.VisualScripting;
+//using UnityEngine;
+//using UnityEngine.UI;
+
+//public class PlayerView : MonoBehaviour
+//{
+//    [SerializeField] TextMeshProUGUI playerName;
+//    [SerializeField] Image playerHp;
+//    [SerializeField] Image voiceImage;
+//    [SerializeField] GameObject[] icon;
+//    [SerializeField] GameObject[] iconOnHand;
+//    [SerializeField] float checkDuration = 0.1f;
+//    Image leftHandIcon;
+//    Image leftHandIconCoolTime;
+//    Image rightHandIcon;
+//    Image rightHandIconCoolTime;
+//    public void SetVoiceImage(Image photonVoiceImage)
+//    {
+//        voiceImage = photonVoiceImage;
+//        voiceImage.enabled = false;
+//    }
+
+//    public void CheckVoiceImage(bool check)
+//    {
+//        voiceImage.enabled = check;
+//    }
+
+//    public void SetPlayerInfo(TextMeshProUGUI name, Image hp, Image voice)
+//    {
+//        playerName = name;
+//        playerHp = hp;
+//        voiceImage = voice;
+//    }
+//    public void SetMyInfo(TextMeshProUGUI name, Image hp)
+//    {
+//        playerName = name;
+//        playerHp = hp;
+//    }
+
+//    public void SetPlayerName(string name)
+//    {
+//        playerName.text = name;
+//    }
+//    public void UpdatePlayerHP(float amount)
+//    {
+//        playerHp.fillAmount = amount;
+//    }
+
+//    public void SetMagicInfo(GameObject[] magic)
+//    {
+//        icon = magic;
+//    }
+
+//    public void SetMagicInfoOnHand(GameObject[] magic)
+//    {
+//        iconOnHand = magic;
+//        leftHandIcon = magic[0].GetComponent<Image>();
+//        rightHandIcon = magic[1].GetComponent<Image>();
+
+//        leftHandIconCoolTime = magic[0].transform.GetChild(0).GetComponent<Image>();
+//        rightHandIconCoolTime = magic[1].transform.GetChild(0).GetComponent<Image>();
+//    }
+
+//    public GameObject GetMagicIcon()
+//    {
+//        foreach (var i in icon)
+//        {
+//            if (!i.activeSelf)
+//                return i;
+//        }
+//        return null;
+//    }
+
+//    public void SetIconOnHand(InventoryDataSO data, bool isLeft)
+//    {
+//        //260116 손 비우기 용 코드 최정욱
+//        if (data == null)
+//        {
+//            if (isLeft)
+//            {
+//                leftHandIcon.sprite = null;
+//                leftHandIconCoolTime.fillAmount = 0;
+//            }
+//            else
+//            {
+//                rightHandIcon.sprite = null;
+//                rightHandIconCoolTime.fillAmount = 0;
+//            }
+//            return;
+//        }
+
+//        if (data.itemImage == null)
+//            return;
+//        if (isLeft)
+//        {
+//            leftHandIcon.sprite = data.itemImage;
+//            leftHandIconCoolTime.fillAmount = 0;
+//        }
+//        else
+//        {
+//            rightHandIcon.sprite = data.itemImage;
+//            rightHandIconCoolTime.fillAmount = 0;
+//        }
+//            //iconOnHand[num].GetComponent<Image>().sprite = data.itemImage;
+
+//    }
+//    public void CheckCoolTimeOnHand(MagicBase magic, bool isLeft)
+//    {
+//        float curCoolTime = magic.CurrentCooldown;
+//        float maxCoolTime = magic.Data.cooldown;
+
+//        Image coolTimeIcon = null;
+//        if (isLeft)
+//            coolTimeIcon = leftHandIconCoolTime;
+//        else
+//            coolTimeIcon = rightHandIconCoolTime;
+
+//        coolTimeIcon.fillAmount = curCoolTime / maxCoolTime;
+//        StartCoroutine(CheckCoolTimeOnHand(coolTimeIcon, curCoolTime, maxCoolTime));
+//    }
+
+
+//    public void SetMagicIcon(MagicBase magic)
+//    {
+//        GameObject magicIcon = GetMagicIcon();
+//        magicIcon.GetComponent<Image>().sprite = magic.Data.itemImage;
+//        Image coolTimeImage = magicIcon.transform.GetChild(0).GetComponent<Image>();
+
+//        float curCoolTime = magic.CurrentCooldown;
+//        float maxCoolTime = magic.Data.cooldown;
+//        coolTimeImage.fillAmount = curCoolTime / maxCoolTime;
+//        magicIcon.SetActive(true);
+//        StartCoroutine(CheckCoolTimeOnInventory(coolTimeImage,curCoolTime,maxCoolTime, magicIcon));        
+//    }
+
+
+
+//    public IEnumerator CheckCoolTimeOnHand(Image checkCoolDown, float curCoolTime, float maxCoolTime)
+//    {
+//        while (curCoolTime > 0)
+//        {
+//            if (curCoolTime > checkDuration)
+//                yield return CoroutineManager.waitForSeconds(checkDuration);
+//            else
+//                yield return CoroutineManager.waitForSeconds(curCoolTime);
+//            curCoolTime -= checkDuration;
+//            checkCoolDown.fillAmount = curCoolTime / maxCoolTime;
+//        }
+//    }
+
+//    public IEnumerator CheckCoolTimeOnInventory(Image checkCoolDown, float curCoolTime, float maxCoolTime, GameObject magicIcon)
+//    {
+//        while (curCoolTime > 0)
+//        {
+//            if (curCoolTime > checkDuration)
+//                yield return CoroutineManager.waitForSeconds(checkDuration);
+//            else
+//                yield return CoroutineManager.waitForSeconds(curCoolTime);
+//            curCoolTime -= checkDuration;
+//            checkCoolDown.fillAmount = curCoolTime / maxCoolTime;
+//        }
+//        if (magicIcon != null)
+//        {
+//            magicIcon.SetActive(false);
+//        }
+//    }
+//}
+#endregion
