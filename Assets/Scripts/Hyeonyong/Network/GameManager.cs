e25289e64245ab01b7d79a1f2bfa995c869a36c9
Yanghyeonyong <didgusdyd2@gmail.com>
2026-01-29 11:43:07 +0900

    Revert "[Edit] 방에서 플레이어 나갈 경우 패배 뜨는 것 방지"
    This reverts commit 62b749e460164f3a6e5475b525daab339e97ee87.

--- Assets/Scripts/Hyeonyong/Network/GameManager.cs
+++ Assets/Scripts/Hyeonyong/Network/GameManager.cs
@@ -210,11 +210,8 @@ public class GameManager : PhotonSingleton<GameManager>
     public override void OnPlayerLeftRoom(Player otherPlayer)
     {
         Debug.Log(otherPlayer.NickName + "님이 게임을 떠났습니다.");
-
         if (PhotonNetwork.IsMasterClient)
         {
-            if (!PhotonNetwork.CurrentRoom.GetProps<bool>(NetworkProperties.ONROOM))
-                return;
             if (otherPlayer.GetProps<bool>(NetworkProperties.PLAYER_ALIVE))
             {
                 int curPlayerCount = PhotonNetwork.CurrentRoom.GetProps<int>(NetworkProperties.PLAYERCOUNT) - 1;
